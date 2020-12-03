f9878f774f679d3782212d84e912f77cce4a0afc
Mochichi2003 <moyurusuto@gmail.com>
2020-12-03 19:22:23 +0900

    色々追加

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.js",
+    "./renderer/**/*.js",
+    "./pages/**/*.jsx",
+    "./renderer/**/*.jsx",
+    "./pages/**/*.ts",
+    "./renderer/**/*.ts",
+    "./pages/**/*.tsx",
+    "./renderer/**/*.tsx",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
