ba7e11e7dda8d1bd288e21c83cff7ccaa15abef9
Mochichi2003 <moyurusuto@gmail.com>
2020-12-08 08:30:56 +0900

    テストを追加

--- renderer/utils/__tests__/Sample.spec.ts
+++ renderer/utils/__tests__/Sample.spec.ts
@@ -8,4 +8,46 @@ describe("Sample.ts Functions TestCases", () => {
 
     expect(result).toStrictEqual(expected);
   });
+  it("should return the squared value", () => {
+    const result = square(4);
+    const expected = 8;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(3);
+    const expected = 6;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(2);
+    const expected = 4;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(2);
+    const expected = 4;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(2);
+    const expected = 4;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(2);
+    const expected = 4;
+
+    expect(result).toStrictEqual(expected);
+  });
+  it("should return the squared value", () => {
+    const result = square(2);
+    const expected = 4;
+
+    expect(result).toStrictEqual(expected);
+  });
 });
